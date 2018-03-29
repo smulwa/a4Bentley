90b2c27c8dbb96c6746efba80f35ad68e7dd0a2d
smulwa <35940989+smulwa@users.noreply.github.com>
2018-03-29 00:44:19 -0400

    findElement works except for searching 26

--- SortAndFind.java
+++ SortAndFind.java
@@ -5,6 +5,7 @@
  * 
  */
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class SortAndFind {
@@ -18,14 +19,24 @@ public class SortAndFind {
 		// sortOneRow(x);
 
 		int[][] y = { { 32, 0, 26 }, { 39, 45, 7 }, { 34, 37, 35 }, { 3, 39, 26 }, { 22, 15, 37 } };
+
+		int[][] z = { { 0, 22, 32 }, { 3, 26, 37 }, { 7, 26, 37 }, { 15, 35, 39 }, { 34, 39, 45 } };
+
+		// displayMatrix(z);
+		// sortMatrix(z);
+		System.out.println();
+		// System.out.println(Arrays.toString(findElement(z, 15)));
+
+		findElement(z, 26);
+		// displayMatrix(z);
+
 		// displayMatrix(sortOneColumn(y, 1));
 
 		// System.out.println();
 
-		/*displayMatrix(y);
-		sortMatrix(y);
-		System.out.println();
-		displayMatrix(y);*/
+		/*
+		 * displayMatrix(y); sortMatrix(y); System.out.println(); displayMatrix(y);
+		 */
 	}
 
 	public static int[][] generateRandomMatrix(int m, int n) {
@@ -103,18 +114,51 @@ public class SortAndFind {
 
 	public static void sortMatrix(int[][] inputArr) {
 
-		//sort rows first
+		// sort rows first
 		for (int i = 0; i < inputArr.length; i++) {
 			sortOneRow(inputArr[i]);
 
 		}
-		
-		//sort columns
+
+		// sort columns
 		for (int j = 0; j < inputArr[0].length; j++) {
 			sortOneColumn(inputArr, j);
 
 		}
 	}
-	public static void 
+
+	public static int[] findElement(int[][] matrix, int n) {
+		// int index = [matrix.length][matrix[0].length];
+
+		int[] answerArray = new int[2];
+		for (int row = matrix.length - 1; row >= 1; row--) {
+			for (int col = 0; col < matrix[0].length; col++) {
+
+				if (matrix[row][col] == n) {
+					// n = matrix[row][col];
+					answerArray[0] = row;
+					answerArray[1] = col;
+					// return answerArray;
+
+				}
+
+				if (matrix[row][col] < n) {
+					row--;
+
+				}
+				if (matrix[row][col] > n) {
+					col++;
+
+				} else if (matrix[row][col] != n) {
+					answerArray[0] = -1;
+					answerArray[1] = -1;
+				}
+
+			}
+		}
+		System.out.print("Index is " + answerArray[0] + ", " + answerArray[1]);
+		return answerArray;
+
+	}
 
 }
